723c62ab50c598e65ffd603fbcade00e10d8ff7b
twocatswalkedintoabar <104509769+twocatswalkedintoabar@users.noreply.github.com>
2022-12-03 21:53:08 -0500

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/halloween_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.halloween_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/button.dart
@@ -0,0 +1,19 @@
+class Button {
+
+  String imageUrl;
+  // TODO: Add servings and ingredients here
+
+  Button(
+    
+    this.imageUrl,
+  );
+  // TODO; Add List<Button> here
+  static List<Button> monsters = [
+  Button('assets/Spider.png'),
+  Button('assets/Bat.jpg'),
+  
+];
+
+}
+
+// TODO: Add Ingredient() here
